e8c2d2b3ac9b2ce86ec36f382851f7484bd63381
Jeffrey van Barneveld <jeffrey@jeffdev.nl>
2016-07-30 11:41:00 +0200

    Running compilebat after build

--- C/compile.bat
+++ C/compile.bat
@@ -1,8 +1,12 @@
 @echo off
 del build\*.o
+echo Assembling..
 "tools\Nasm\nasm.exe" -f elf32 load.asm -o build\load.o
 "tools\Nasm\nasm.exe" -f elf32 portio.asm -o build\portio.o
 "tools\Nasm\nasm.exe" -f elf32 arch.asm -o build\arch.o
+echo Compiling..
 "tools\gcc\bin\i586-elf-gcc.exe" -I./include -m32 -Wall -O2 -std=c99 -nostdlib kernel.c -c -o build\kernel.o
+echo Linking kernel...
 "tools\gcc\bin\i586-elf-ld.exe" -Tlinker.ld -s build\*.o -o kernel
 copy kernel G:\kernel > NUL
+pause;
