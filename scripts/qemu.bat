7ae8d695922938697f915c9618ad75b1f1be63e7
Jeffrey van Barneveld <jeffrey@jeffdev.nl>
2016-10-17 13:28:00 +0200

    Removed files

--- scripts/qemu.bat
+++ scripts/qemu.bat
@@ -1,4 +1,4 @@
 echo Starting QEMU...
 rem "C:\Program Files\qemu\qemu-system-i386.exe" os.img -soundhw ac97 -net nic,model=rtl8139 -serial file:test.txt -net tap,ifname=TAP -m 256 -D qemu.log
-"C:\Program Files\qemu\qemu-system-i386.exe" ..\..\os.img -soundhw ac97 -net nic,model=pcnet -serial file:test.txt  -m 256 -D qemu.log -net tap,ifname=TAP
+"C:\Program Files\qemu\qemu-system-i386.exe" ..\..\os.img -soundhw ac97 -net nic,model=pcnet -serial file:serial.txt  -m 256 -D qemu.log -net tap,ifname=TAP
 rem net dump,file=netdump.wcap  
