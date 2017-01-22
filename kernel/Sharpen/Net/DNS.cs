0bad7a5ab348846a8edbb24e28178398a843c06c
jeffr <jeffrey@jeffdev.nl>
2017-01-22 10:49:53 +0100

    Begin of DNS

---
+++ kernel/Sharpen/Net/DNS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpen.Net
+{
+    class DNS
+    {
+
+        const ushort FLAG_QR_RESPONSE = (1 << 0);
+        const ushort FLAG_AA_AUTH = (1 << 5);
+        const ushort FLAG_TC_TRUNC = (1 << 6);
+        const ushort FLAG_RD__DES = (1 << 7);
+        const ushort FLAG_RA_AVAI = (1 << 8);
+        const ushort FLAG_AD_AUTH = (1 << 10);
+        const ushort FLAG_CD_CHECK = (1 << 11);
+
+        const ushort FLAG_RCODE = (1 << 12);
+    }
+}
---
+++ kernel/Sharpen/Net/DNSExtras.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpen.Net
+{
+    struct DNSHeader
+    {
+        public ushort Identification;
+        public ushort Flags;
+        public ushort TotalQuestions;
+        public ushort TotalAnsswersRR;
+        public ushort TotalAuthorityRR;
+        public ushort TotalAdditionalRR;
+    }
+
+    enum DNSQueryResponse: byte
+    {
+        Query,
+        Response
+    }
+
+    enum DNSOpcode
+    {
+        QUERY,
+        IQUERY,
+        STATUS,
+        BLANK,
+        NOTIFY,
+        UPDATE
+    }
+
+    enum DDAA: byte
+    {
+        NotAuthoritative,
+        Authoritative
+    }
+}
