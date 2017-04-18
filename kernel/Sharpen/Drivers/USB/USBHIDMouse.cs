99f292502b02a5c34246acacb7f97448c053dd2b
jeffreyvanbarneveld <jeffrey@jeffdev.nl>
2017-04-18 12:19:23 +0200

    Work on HUBs

--- kernel/Sharpen/Drivers/USB/USBHIDMouse.cs
+++ kernel/Sharpen/Drivers/USB/USBHIDMouse.cs
@@ -3,12 +3,14 @@ using Sharpen.USB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Sharpen.Drivers.USB
 {
 
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct MouseLocation
     {
         public byte Buttons;
---
+++ kernel/Sharpen/Drivers/USB/USBHub.cs
@@ -0,0 +1,58 @@
+﻿using Sharpen.USB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpen.Drivers.USB
+{
+    unsafe class USBHub : IUSBDriver
+    {
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct USBHubDescriptor
+        {
+            public byte Length;
+            public byte Type;
+            public byte NumPorts;
+            public ushort Characterstics;
+            public byte PWR;
+            public byte ControlConcurrent;
+            public byte DeviceRemovable;
+            public byte PWRMask;
+        }
+
+        private USBHubDescriptor *descriptor;
+
+        public void Close(USBDevice device)
+        {
+
+        }
+
+        public unsafe void Init(USBDevice device)
+        {
+
+        }
+
+        public unsafe IUSBDriver Load(USBDevice device)
+        {
+            if(device.InterfaceDesc->Class == (byte)USBClassCodes.HUB)
+            {
+                USBHub hub = new USBHub();
+                hub.Init(device);
+
+                device.Classifier = USBDeviceClassifier.HUB;
+
+                return hub;
+            }
+
+            return null;
+        }
+
+        public void Poll(USBDevice device)
+        {
+
+        }
+    }
+}
