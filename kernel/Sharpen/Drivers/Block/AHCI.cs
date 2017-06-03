b2db4f408576b4b168747e735017b5e7f74b950a
jeffrey van Barneveld <jeffrey@jeffdev.nl>
2017-06-03 21:54:01 +0200

    [AHCI] Start

--- kernel/Sharpen/Drivers/Block/AHCI.cs
+++ kernel/Sharpen/Drivers/Block/AHCI.cs
@@ -1,4 +1,5 @@
 ﻿using Sharpen.Arch;
+using Sharpen.Mem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,28 +8,144 @@ using System.Threading.Tasks;
 
 namespace Sharpen.Drivers.Block
 {
-    class AHCI
+
+    unsafe struct AHCI_Port_registers
     {
+        public uint CLB;
+        public uint CLBU;
+        public uint FB;
+        public uint FBU;
+        public uint IS;
+        public uint IE;
+        public uint CMD;
+        public uint Res;
+        public uint TFD;
+        public uint SIG;
+        public uint SSTS;
+        public uint SCTL;
+        public uint SERR;
+        public uint SACT;
+        public uint CI;
+        public uint SNTF;
+        public uint Fbs;
+        public fixed uint Res2[11];
+        public fixed uint Vendor[4];
+    }
 
-        public static void Init()
+    unsafe struct AHCI_Generic_registers
+    {
+        public uint CAP;
+        public uint GHC;
+        public uint IS;
+        public uint PI;
+        public uint VS;
+        public uint CCC_CTL;
+        public uint CCC_PORTS;
+        public uint EM_LOC;
+        public uint EM_CTL;
+        public uint CAP2;
+        public uint BOHC;
+
+        public fixed byte Res[116];
+
+        public fixed byte Vendor[96];
+    }
+
+    unsafe struct AHCI_Command_header
+    {
+        // DW 0 - Description info
+        public ushort Options;
+        public ushort Prdtl;
+
+        // DW1 - Command status
+        public uint PRDBC;
+
+        // DW2 - Command table base address
+        public uint CTBA;
+
+        // DW3 - Command table base address upper
+        public uint CTBUA;
+
+        public fixed uint Res[4];
+    }
+
+    unsafe class AHCI
+    {
+        private const int NUM_CMD_HEADERS = 32;
+
+        private const int CAP_NUM_PORTS_MASK = 0x1F;
+
+        private static int mID;
+
+        private PciDevice mPciDevice;
+        private int mAddress;
+        private int mAddressVirtual;
+
+        private AHCI_Generic_registers* mGenericRegs;
+        private AHCI_Port_registers* mPorts;
+        private uint mSupportedPorts;
+
+        private AHCI_Command_header* CmdHeaders;
+
+        public AHCI(PciDevice dev)
         {
-            PciDevice device = findAHCIDevice();
+            mPciDevice = dev;
+        }
 
-            if (device != null)
+        public unsafe void InitDevice()
+        {
+            /**
+             * Check if there is a memory bar
+             */
+            if ((mPciDevice.BAR5.flags & Pci.BAR_IO) > 0)
             {
-                Console.WriteLine("[AHCI] Device detected, pauzing kernel...");
-                for (;;) ;
+                Console.WriteLine("[E1000] Device not MMIO!");
+                return;
             }
-            
+
+            Pci.EnableBusMastering(mPciDevice);
+
+
+            int size = sizeof(AHCI_Generic_registers) + (sizeof(AHCI_Port_registers) * 32);
+            mAddress = (int)mPciDevice.BAR5.Address;
+            mAddressVirtual = (int)Paging.MapToVirtual(Paging.CurrentDirectory, mAddress, size, Paging.PageFlags.Writable | Paging.PageFlags.Present);
+
+            mGenericRegs = (AHCI_Generic_registers*)mAddressVirtual;
+            mPorts = (AHCI_Port_registers *)(mAddressVirtual + 0x100);
+
+            ReadCapabilties();
+            initPorts();
+
+        }
+
+        private void ReadCapabilties()
+        {
+            mSupportedPorts = mGenericRegs->CAP & CAP_NUM_PORTS_MASK;
+            mSupportedPorts++;
+        }
+
+        private void initPorts()
+        {
+            for (int i = 0; i < mSupportedPorts; i++)
+                initPort(i);
+        }
+
+        private void initPort(int portNum)
+        {
+
+            AHCI_Port_registers* portReg = mPorts + portNum;
+
+
+            // Note: 1k alligned?
+            CmdHeaders = (AHCI_Command_header *) Heap.Alloc(sizeof(AHCI_Command_header) * NUM_CMD_HEADERS);
 
 
         }
 
-
-
-
-        private static PciDevice findAHCIDevice()
+        public static void Init()
         {
+            mID = 0;
+
             /**
              * Note: this cycles through PCI devices!
              */
@@ -37,10 +154,11 @@ namespace Sharpen.Drivers.Block
                 PciDevice dev = Pci.Devices[i];
 
                 if (dev.CombinedClass == 0x0106 && dev.ProgIntf == 0x01)
-                    return dev;
+                {
+                    AHCI ahci = new AHCI(dev);
+                    ahci.InitDevice();
+                }
             }
-
-            return null;
         }
     }
 }
--- scripts/qemu.bat
+++ scripts/qemu.bat
@@ -5,5 +5,5 @@ rem "C:\Program Files\qemu\qemu-system-i386.exe" -drive id=disk,file=..\..\os.im
 rem -usbdevice disk:..\..\usbb.img
 rem -drive if=none,id=usbstick,file=..\..\usbb.img -device usb-uhci,id=uhci -device usb-storage,bus=uhci.0,drive=usbstick
 
-"C:\Program Files\qemu\qemu-system-i386.exe" ..\..\os.img -soundhw ac97 -net nic,model=e1000 -serial file:serial.txtd  -m 256 -D qemu.log  -net tap,ifname=TAP
+"C:\Program Files\qemu\qemu-system-i386.exe" -soundhw ac97 -net nic,model=e1000 -serial file:serial.txtd  -m 256 -D qemu.log  -drive id=disk,file=..\..\os.img,if=none -device ahci,id=ahci -device ide-drive,drive=disk,bus=ahci.0 
 rem net dump,file=netdump.wcap
