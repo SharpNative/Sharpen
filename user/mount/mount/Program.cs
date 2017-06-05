6013986c772d0343e018d36645c1f819aa634ca0
jeffrey van Barneveld <jeffrey@jeffdev.nl>
2017-06-05 19:44:16 +0200

    Mount/Umount programs

---
+++ user/mount/mount/Program.cs
@@ -0,0 +1,49 @@
+﻿using LibCS2C.Attributes;
+using Sharpen.IO;
+using Sharpen.Mem;
+
+namespace mount
+{
+    class Program
+    {
+        [Extern("mount", true)]
+        public static extern int mount(string devicePath, string mountName, string fsType);
+
+        /// <summary>
+        /// Entrypoint
+        /// </summary>
+        /// <param name="args">The arguments</param>
+        [Plug("EntryPoint")]
+        static void Main(string[] args)
+        {
+
+            // Get argument count
+            int argc = 0;
+            for (; args[argc] != null; argc++) ;
+
+            // Usage
+            if (argc != 4)
+            {
+                Console.WriteLine("mount: Mount a device on a root point");
+                Console.WriteLine("Usage: mount [sourceFile] [targetMount] [FsType]");
+                return;
+            }
+
+            int result = mount(args[1], args[2], args[3]);
+
+            if (result == 0)
+            {
+                Console.Write(args[1]);
+                Console.Write(" mounted on ");
+                Console.Write(args[2]);
+                Console.WriteLine("://");
+            }
+            else if (result == -20)
+                Console.WriteLine("Target not a mountpoint.");
+            else if (result == -19)
+                Console.WriteLine("Given filesystem type not right");
+            else if (result == -2)
+                Console.WriteLine("Target already used");
+        }
+    }
+}
---
+++ user/umount/umount/Program.cs
@@ -0,0 +1,40 @@
+﻿using LibCS2C.Attributes;
+using Sharpen.IO;
+using Sharpen.Mem;
+
+namespace mount
+{
+    class Program
+    {
+        [Extern("umount", true)]
+        public static extern int umount(string mountName);
+
+        /// <summary>
+        /// Entrypoint
+        /// </summary>
+        /// <param name="args">The arguments</param>
+        [Plug("EntryPoint")]
+        static void Main(string[] args)
+        {
+
+            // Get argument count
+            int argc = 0;
+            for (; args[argc] != null; argc++) ;
+
+            // Usage
+            if (argc != 2)
+            {
+                Console.WriteLine("mount: Unmount a mountpoint");
+                Console.WriteLine("Usage: umount [targetMount]");
+                return;
+            }
+
+            int result = umount(args[1]);
+
+            if (result == 0)
+                Console.WriteLine("Target unmounted.");
+            else
+                Console.WriteLine("Could not unmount target.");
+        }
+    }
+}
